1892aaad2be66b09c7d934cafc9508d189b151c0
stevo123 <fritzq@mindspring.com>
2024-12-04 00:17:23 -0500

    Completed through step 4

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -2,23 +2,42 @@ import React, { useState } from 'react';
 import './TodoList.css';
 
 const TodoList = () => {
+    const [todos, setTodos] = useState([]);
+    const [headingInput, setHeadingInput] = useState('');
+    const [listInputs, setListInputs] = useState({});
+
+    const handleAddTodo = () => {
+        if (headingInput.trim() !== '') {
+            setTodos([...todos, { heading: headingInput, lists: [] }]);
+            setHeadingInput('');
+        }
+    };
   
 
   return (
     <>
       <div className="todo-container">
         <h1 className="title">My Todo List</h1>
-        <div className="input-container">
-          <input
-            type="text"
-            className="heading-input"
-            placeholder="Enter heading"
-            
-          />
-          <button className="add-list-button">Add Heading</button>
-        </div>
+            <div className="input-container">
+            <input
+                type="text"
+                className="heading-input"
+                placeholder="Enter heading"
+                value={headingInput}
+                onChange={(e) => {setHeadingInput(e.target.value);}} // Add onChange event handler to update headingInput state
+            />
+                <button className="add-list-button" onClick={handleAddTodo}>Add Heading</button>
+            </div>
       </div>
       <div className="todo_main">
+      {todos.map((todo, index) => (
+        <div key={index} className="todo-card">
+        <div className="heading_todo">
+            <h3>{todo.heading}</h3> {/* Display the heading here */}
+            <button className="delete-button-heading" onClick={() => handleDeleteTodo(index)}>Delete Heading </button>
+        </div>
+        </div>
+    ))}
         
       </div>
     </>
